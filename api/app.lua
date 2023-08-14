8251ed930c8222e1968869ff5c4de4cfd5933ed8
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-13 22:17:12 -0300

    Insert person

--- api/app.lua
+++ api/app.lua
@@ -2,28 +2,29 @@ local lapis = require("lapis")
 local console = require("lapis.console")
 local Model = require("lapis.db.model").Model
 local json_params = require("lapis.application").json_params
-local capture_errors_json = require("lapis.application").capture_errors_json
+local custom_capture_errors_json = require("exception").custom_capture_errors_json
 local with_params = require("lapis.validate").with_params
 local types = require("lapis.validate.types")
+local to_json = require("lapis.util").to_json
 local custom_types = require("validators.types")
 
 local app = lapis.Application()
 
 local People = Model:extend("pessoas")
 
-app:post("/", capture_errors_json(json_params(with_params({
+app:post("/", custom_capture_errors_json(422, json_params(with_params({
   { "apelido",    types.limited_text(256) },
   { "nome",       types.limited_text(100) },
   { "nascimento", custom_types.is_valid_date },
   { "stack",      types.array_of(types.limited_text(32)) }
 }, function(self, params)
-  -- local person = People:create({
-  --   apelido = self.params.apelido,
-  --   nome = self.params.nome,
-  --   nascimento = self.params.nascimento,
-  --   stack = self.params.stack
-  -- })
-  print("foo")
+  local person = People:create({
+    apelido = params.apelido,
+    nome = params.nome,
+    nascimento = params.nascimento,
+    stack = to_json(params.stack)
+  })
+  return "OK"
 end))))
 
 app:match("/console", console.make())
--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS pessoas (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     apelido VARCHAR(32) UNIQUE NOT NULL,
     nome VARCHAR(100) NOT NULL,
     nascimento DATE NOT NULL,
