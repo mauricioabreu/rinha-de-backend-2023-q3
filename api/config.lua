e72ce2ef10f4a9b2447de5897cb9b3a00c8b52fa
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-15 00:18:17 -0300

    Allow running using production configs

--- api/config.lua
+++ api/config.lua
@@ -1,18 +1,30 @@
 local config = require("lapis.config")
 
-config("development", {
+config({ "development", "production" }, {
   port = 80,
   server = "nginx",
-  code_cache = "off",
-  num_workers = "1",
   postgres = {
     host = "db",
     user = "rinhadebackend",
     password = "rinhadebackend",
     database = "rinhadebackend"
-  },
+  }
+})
+
+config("development", {
+  num_workers = "1",
+  code_cache = "off",
   logging = {
     queries = true,
     requests = true,
+  }
+})
+
+config("production", {
+  num_workers = "4",
+  code_cache = "on",
+  logging = {
+    queries = false,
+    requests = false,
   }
 })
