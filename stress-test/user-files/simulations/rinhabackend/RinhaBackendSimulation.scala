f2170cb2b1cfbacdc3c532e164eb6eb981cce031
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-20 07:43:37 -0300

    Less stress

--- stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
+++ stress-test/user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
@@ -54,12 +54,12 @@ class RinhaBackendSimulation
       constantUsersPerSec(2).during(10.seconds), // warm up
       constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
 
-      rampUsersPerSec(6).to(320).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(300).during(3.minutes) // lezzz go!!!
     ),
     buscaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
 
-      rampUsersPerSec(6).to(60).during(3.minutes) // lezzz go!!!
+      rampUsersPerSec(6).to(50).during(3.minutes) // lezzz go!!!
     ),
     buscaInvalidaPessoas.inject(
       constantUsersPerSec(2).during(25.seconds), // warm up
